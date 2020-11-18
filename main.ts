a88e6cc5d203e674f2f4162284ee3269933175d2
Eric Hall <me@erichall.io>
2020-11-18 10:22:33 -0600

    add eslint and prettier

--- main.ts
+++ main.ts
@@ -1,125 +1,122 @@
 import { App, ButtonComponent, DropdownComponent, Modal, Plugin } from 'obsidian';
 
 export default class SmartRandomNotePlugin extends Plugin {
-	async onload(): Promise<void> {
-		console.log('loading smart-random-note');
+    async onload(): Promise<void> {
+        console.log('loading smart-random-note');
 
-		this.addCommand({
-			id: 'open-random-note',
-			name: 'Open Random Note',
-			callback: this.openRandomNote,
-		})
+        this.addCommand({
+            id: 'open-random-note',
+            name: 'Open Random Note',
+            callback: this.openRandomNote,
+        });
 
-		this.addCommand({
-			id: 'open-tagged-random-note',
-			name: 'Open Tagged Random Note',
-			callback: this.openTaggedRandomNote,
-		})
-	}
+        this.addCommand({
+            id: 'open-tagged-random-note',
+            name: 'Open Tagged Random Note',
+            callback: this.openTaggedRandomNote,
+        });
+    }
 
-	onunload = (): void => {
-		console.log('unloading smart-random-note');
-	}
-	
-	openRandomNote = async (): Promise<void> => {
-		const markdownFiles = this.app.vault.getMarkdownFiles();
+    onunload = (): void => {
+        console.log('unloading smart-random-note');
+    };
 
-		var filePaths = markdownFiles.map(x => x.path);
-		const filePathToOpen = randomElement(filePaths);
-		await this.app.workspace.openLinkText(filePathToOpen, '', true, { active: true });
-	};
+    openRandomNote = async (): Promise<void> => {
+        const markdownFiles = this.app.vault.getMarkdownFiles();
 
-	openTaggedRandomNote = (): void => {
-		const tagFilesMap = getTagFilesMap(this.app);
+        const filePaths = markdownFiles.map((x) => x.path);
+        const filePathToOpen = randomElement(filePaths);
+        await this.app.workspace.openLinkText(filePathToOpen, '', true, { active: true });
+    };
 
-		const tags = Object.keys(tagFilesMap);
-		const modal = new SmartRandomNoteModal(this.app, tags);
+    openTaggedRandomNote = (): void => {
+        const tagFilesMap = getTagFilesMap(this.app);
 
-		modal.submitCallback = async (): Promise<void> => {
-			const taggedFiles = tagFilesMap[modal.selectedTag];
-			const fileNameToOpen = randomElement(taggedFiles);
-			await this.app.workspace.openLinkText(fileNameToOpen, '', true, { active: true });
-		}
+        const tags = Object.keys(tagFilesMap);
+        const modal = new SmartRandomNoteModal(this.app, tags);
 
-		modal.open();
-	};
+        modal.submitCallback = async (): Promise<void> => {
+            const taggedFiles = tagFilesMap[modal.selectedTag];
+            const fileNameToOpen = randomElement(taggedFiles);
+            await this.app.workspace.openLinkText(fileNameToOpen, '', true, { active: true });
+        };
+
+        modal.open();
+    };
 }
 
-type TagFileMap = { [tag: string]: string[] }
+type TagFilesMap = { [tag: string]: string[] };
 
-function getTagFilesMap(app: App): TagFileMap {
-	const metadataCache = app.metadataCache;
-	const markdownFiles = app.vault.getMarkdownFiles();
-	const tagFilesMap: { [tag: string]: string[] } = {};
+function getTagFilesMap(app: App): TagFilesMap {
+    const metadataCache = app.metadataCache;
+    const markdownFiles = app.vault.getMarkdownFiles();
 
-	for (var markdownFile of markdownFiles) {
-		const cachedMetadata = metadataCache.getFileCache(markdownFile);
-		
-		if (cachedMetadata && cachedMetadata.tags) {
-			for (var cachedTag of cachedMetadata.tags) {
-				if (tagFilesMap[cachedTag.tag]) {
-					tagFilesMap[cachedTag.tag].push(markdownFile.path);
-				} else {
-					tagFilesMap[cachedTag.tag] = [markdownFile.path];
-				}
-			}
-		}
-	}
+    const tagFilesMap: TagFilesMap = {};
 
-	return tagFilesMap;
+    for (const markdownFile of markdownFiles) {
+        const cachedMetadata = metadataCache.getFileCache(markdownFile);
+
+        if (cachedMetadata && cachedMetadata.tags) {
+            for (const cachedTag of cachedMetadata.tags) {
+                if (tagFilesMap[cachedTag.tag]) {
+                    tagFilesMap[cachedTag.tag].push(markdownFile.path);
+                } else {
+                    tagFilesMap[cachedTag.tag] = [markdownFile.path];
+                }
+            }
+        }
+    }
+
+    return tagFilesMap;
 }
 
 function randomElement<T>(array: T[]): T {
-	return array[array.length * Math.random() << 0];
+    return array[(array.length * Math.random()) << 0];
 }
 
 class SmartRandomNoteModal extends Modal {
-	tags: string[];
-	selectedTag: string = '';
-	firstKeyUpHandled: boolean = false;
-	submitCallback: () => Promise<void> = async () => {};
+    tags: string[];
+    selectedTag = '';
+    firstKeyUpHandled = false;
+    submitCallback: (() => Promise<void>) | undefined = undefined;
 
-	constructor(app: App, tags: string[]) {
-		super(app);
-		this.tags = tags;
-		this.selectedTag = tags[0];
-	}
+    constructor(app: App, tags: string[]) {
+        super(app);
+        this.tags = tags;
+        this.selectedTag = tags[0];
+    }
 
-	onOpen = () => {
-		this.contentEl.createEl('h3', { text: 'Select Tag' });
+    onOpen = () => {
+        this.contentEl.createEl('h3', { text: 'Select Tag' });
 
-		const tagDropdown = new DropdownComponent(this.contentEl)
-			.onChange(value => this.selectedTag = value);
+        const tagDropdown = new DropdownComponent(this.contentEl).onChange((value) => (this.selectedTag = value));
 
-		for (var tag of this.tags) {
-			tagDropdown.addOption(tag, tag);
-		}
+        for (const tag of this.tags) {
+            tagDropdown.addOption(tag, tag);
+        }
 
-		tagDropdown.setValue(this.selectedTag);
+        tagDropdown.setValue(this.selectedTag);
 
-		new ButtonComponent(this.contentEl)
-			.setButtonText('Submit')
-			.setCta()
-			.onClick(this.submit);
+        new ButtonComponent(this.contentEl).setButtonText('Submit').setCta().onClick(this.submit);
 
-		document.addEventListener('keyup', this.handleKeyUp);
-	};
+        document.addEventListener('keyup', this.handleKeyUp);
+    };
 
-	handleKeyUp = (event: KeyboardEvent): void => {
-		if (this.firstKeyUpHandled && event.key == 'Enter') {
-			this.submit();
-		}
-		this.firstKeyUpHandled = true;
-	}
+    handleKeyUp = (event: KeyboardEvent): void => {
+        if (this.firstKeyUpHandled && event.key == 'Enter') {
+            this.submit();
+        }
+        this.firstKeyUpHandled = true;
+    };
 
-	submit = (): void => {
-		if (this.submitCallback) {
-			this.submitCallback();
-		}
-		this.close();
-	};
+    submit = async (): Promise<void> => {
+        if (this.submitCallback) {
+            await this.submitCallback();
+        }
+        this.close();
+    };
 
-	onClose = (): void => {
-		document.removeEventListener('keyup', this.handleKeyUp);
-	};
+    onClose = (): void => {
+        document.removeEventListener('keyup', this.handleKeyUp);
+    };
 }
