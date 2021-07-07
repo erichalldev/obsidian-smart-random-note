f90a33f6b419f97a1f9aca6ef90e4ad7e06a3213
Eric Hall <me@erichall.io>
2021-07-07 14:55:46 -0500

    Add command to insert a link at the cursor to a random note from search.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin, TFile } from 'obsidian';
+import { MarkdownView, Plugin, TFile } from 'obsidian';
 import { getTagFilesMap, randomElement } from './utilities';
 import { SmartRandomNoteSettingTab } from './settingTab';
 import { SearchView, SmartRandomNoteSettings } from './types';
@@ -32,6 +32,12 @@ export default class SmartRandomNotePlugin extends Plugin {
             id: 'open-random-note-from-search',
             name: 'Open Random Note from Search',
             callback: this.handleOpenRandomNoteFromSearch,
+        });
+
+        this.addCommand({
+            id: 'insert-link-to-random-note-at-cursor',
+            name: 'Insert Link at Cursor to Random Note from Search',
+            callback: this.handleInsertLinkFromSearch,
         });
     }
 
@@ -77,11 +83,29 @@ export default class SmartRandomNotePlugin extends Plugin {
         await this.openRandomNote(searchResults);
     };
 
+    handleInsertLinkFromSearch = async (): Promise<void> => {
+        const searchView = this.app.workspace.getLeavesOfType('search')[0]?.view as SearchView;
+
+        if (!searchView) {
+            new SmartRandomNoteNotice('The core search plugin is not enabled', 5000);
+            return;
+        }
+
+        const searchResults = searchView.dom.getFiles();
+
+        if (!searchResults.length) {
+            new SmartRandomNoteNotice('No search results available', 5000);
+            return;
+        }
+
+        await this.insertRandomLinkAtCursor(searchResults);
+    };
+
     openRandomNote = async (files: TFile[]): Promise<void> => {
         const markdownFiles = files.filter((file) => file.extension === 'md');
 
         if (!markdownFiles.length) {
-            new SmartRandomNoteNotice("Can't open note. No markdown files found in search results", 5000);
+            new SmartRandomNoteNotice("Can't open note. No markdown files available to open.", 5000);
             return;
         }
 
@@ -89,6 +113,27 @@ export default class SmartRandomNotePlugin extends Plugin {
         await this.app.workspace.openLinkText(fileToOpen.basename, '', this.settings.openInNewLeaf, {
             active: true,
         });
+    };
+
+    insertRandomLinkAtCursor = async (files: TFile[]): Promise<void> => {
+        const fileToLink = randomElement(files);
+        const activeLeaf = this.app.workspace.activeLeaf;
+        if (!activeLeaf) {
+            new SmartRandomNoteNotice("Can't insert link. No active note to insert link into", 5000);
+            return;
+        }
+        const viewState = activeLeaf.getViewState();
+        const canEdit = viewState.type === 'markdown' && viewState.state && viewState.state.mode == 'source';
+
+        if (!canEdit) {
+            new SmartRandomNoteNotice("Can't insert link. The active file is not a markdown file in edit mode.", 5000);
+            return;
+        }
+
+        const markdownView = activeLeaf.view as MarkdownView;
+        const cursorPos = markdownView.editor.getCursor();
+        const textToInsert = `[[${fileToLink.name}]]`;
+        markdownView.editor.replaceRange(textToInsert, cursorPos);
     };
 
     loadSettings = async (): Promise<void> => {
